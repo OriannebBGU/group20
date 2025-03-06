b4dfc42a360893c557b2624b42a73f25b8af5ea1
shai tsfaty <shaitsf@post.bgu.ac.il>
2025-03-06 14:34:41 +0200

    login only

--- pages/login/login.py
+++ pages/login/login.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, flash, url_for, redirect
+from group20.db_connector import get_customer_by_email
 
 # login blueprint definition
 login = Blueprint(
@@ -11,6 +12,22 @@ login = Blueprint(
 
 
 # Routes
-@login.route('/login')
+@login.route('/login', methods=['GET', 'POST'])
 def login_func():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Query the database for the customer by email
+        user = get_customer_by_email(email)
+
+        # Check if the user exists and if the password matches
+        if user and user.get('Password') == password:
+            session['logged_in'] = True
+            session['user_email'] = email
+            session['user_role'] = user.get('Role')
+            return redirect(url_for('homepage.homepage_func'))
+        else:
+            flash('Invalid email or password.')
+            return redirect(url_for('login.login_func'))
     return render_template('login.html')
